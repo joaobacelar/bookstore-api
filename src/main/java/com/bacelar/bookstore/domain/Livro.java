0fd636a73a9f0bbe0f89852760eedd0a97d62495
joaobacelar <joao.bacelar22@gmail.com>
2022-05-17 21:28:31 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/bacelar/bookstore/domain/Livro.java
@@ -0,0 +1,12 @@
+package com.bacelar.bookstore.domain;
+
+public class Livro {
+	
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+	
+	private Categoria categoria;
+
+}
